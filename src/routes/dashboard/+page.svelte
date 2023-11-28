f854768a9d20e3fea14308d53ea838ce6ca3f9b2
amiftachulh <miftachul.mkb@gmail.com>
2023-11-28 13:37:50 +0700

    Update rent modal

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,14 +1,40 @@
 <script lang="ts">
-  import { Button, Input, Modal } from "flowbite-svelte";
+  import { Button, Input, Modal, Select } from "flowbite-svelte";
   import type { PageData } from "./$types";
   import Footer from "$lib/components/Footer.svelte";
+  import { onMount } from "svelte";
 
   export let data: PageData;
   let carModals: boolean[] = Array(data.cars.length).fill(false);
+  let selected: number;
+  let totalCost: number;
 
-  function openModal(index: number) {
-    carModals[index] = true;
+  function updateTotalCost() {
+    if (selected !== undefined) {
+      totalCost = selected * data.cars[selected].costPerDay;
+      console.log(data.cars[selected - 1]);
+    }
   }
+
+  function openModal(carId: number) {
+    carModals[carId] = true;
+    selected = 1;
+    updateTotalCost();
+  }
+
+  let days = [
+    { value: 1, name: "1 hari" },
+    { value: 2, name: "2 hari" },
+    { value: 3, name: "3 hari" },
+    { value: 4, name: "4 hari" },
+    { value: 5, name: "5 hari" },
+    { value: 6, name: "6 hari" },
+    { value: 7, name: "7 hari" },
+  ];
+
+  onMount(() => {
+    updateTotalCost();
+  });
 </script>
 
 <svelte:head>
@@ -47,16 +73,20 @@
       >
         <form class="flex flex-col space-y-6" action="#">
           <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-            Sewa {car.name}
+            Sewa <span class="font-bold">{car.name}</span>
           </h3>
-          <Input
-            type="text"
-            id="username"
-            name="username"
-            placeholder="Nama Pengguna"
-            autocomplete="off"
-            required
+          <Select
+            class="mt-2"
+            items={days}
+            bind:value={selected}
+            on:change={updateTotalCost}
           />
+          <p class="font-bold">
+            {new Intl.NumberFormat("id-ID", {
+              style: "currency",
+              currency: "IDR",
+            }).format(totalCost)}
+          </p>
         </form>
       </Modal>
     {/each}
